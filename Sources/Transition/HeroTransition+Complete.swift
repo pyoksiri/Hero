e5882df40ecfa9db1fc183bd67c386e51aa0ca5a
Phanuwat Yoksiri <ibugattiz@aol.com>
2018-02-19 18:10:53 +0700

    update toggle status bar

--- Sources/Transition/HeroTransition+Complete.swift
+++ Sources/Transition/HeroTransition+Complete.swift
@@ -86,6 +86,9 @@ extension HeroTransition {
       if isPresenting != finished, !inContainerController, transitionContext != nil {
         // only happens when present a .overFullScreen VC
         // bug: http://openradar.appspot.com/radar?id=5320103646199808
+        if UIApplication.shared.statusBarFrame.height == 40.0 {
+          toView.frame.origin.y = 40.0
+        }
         UIApplication.shared.keyWindow?.addSubview(isPresenting ? fromView : toView)
       }
     }
